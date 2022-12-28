a82c98fd70318f121d9ca828c9d7cf5f48aef2ea
Nabid Kaisar <himelkaisar023@gmail.com>
2022-12-28 11:20:39 +0600

    added socket initial codes

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from "express";
-
+import { Server } from "socket.io";
 import initializeServer from "./initializeServer";
+import * as http from "http";
 
+//here app is an instance of express
 const app = initializeServer();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const PORT: number = 3001;
+
+io.on("connection", (sokcket) => {
+  console.log("new user connected");
+});
 
 app.use("/", (req: Request, res: Response): void => {
   res.send("Hello world!!!");
